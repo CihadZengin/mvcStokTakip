d754edb24675f8b919457bc108da6f38ca9e94ec
CihadZengin <mcihadz@gmail.com>
2025-10-20 12:15:14 +0300

    StokTakip
    Dersler İle Almış Olduğum Ve Yaptıgım mvc Stok Takibi

---
+++ BonusMvcStok/BonusMvcStok/Controllers/GirisYapController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Common.CommandTrees;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BonusMvcStok.Models.Entity;
+using System.Web.Security;
+using System.Data.Entity.Core.Metadata.Edm;
+namespace BonusMvcStok.Controllers
+    
+{
+    public class GirisYapController : Controller
+    {
+        // GET: GirisYap
+        DbMvcStokEntities db = new DbMvcStokEntities();
+        public ActionResult Giris()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Giris(tbladmin t)
+        {
+            var bilgiler = db.tbladmin.FirstOrDefault(x => x.Kullanici == t.Kullanici && x.sifre == t.sifre);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.Kullanici, false);
+                return RedirectToAction("Index","Musteri");
+            }
+            else
+            {
+                return View();
+
+            }
+
+        }
+    }
+}
---
+++ BonusMvcStok/BonusMvcStok/Controllers/SatislarController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BonusMvcStok.Models.Entity;
+
+namespace BonusMvcStok.Controllers
+{
+    public class SatislarController : Controller
+    {
+        // GET: Satislar
+        DbMvcStokEntities db = new DbMvcStokEntities();
+        [Authorize]
+        public ActionResult Index()
+        {
+            var satislar = db.tblsatislar.ToList();
+            return View(satislar);
+        }
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            //Urunler
+            List<SelectListItem> urun = (from x in db.tblurunler.ToList()
+                                         select new SelectListItem
+                                         {
+                                             Text = x.ad,
+                                             Value = x.id.ToString()
+                                         }).ToList();
+            ViewBag.drop1 = urun;
+            //urunfiyat
+            List<SelectListItem> urunfiyat = (from x in db.tblurunler.ToList()
+                                         select new SelectListItem
+                                         {
+                                             Text = x.satisfiyat.ToString(),
+                                             Value = x.id.ToString(),
+                                         }).ToList();
+            ViewBag.drop4 = urunfiyat;
+            //Personel
+            List<SelectListItem> per = (from x in db.tblpersonel.ToList()
+                                        select new SelectListItem
+                                        {
+                                            Text = x.ad + " " + x.soyad,
+                                            Value = x.id.ToString()
+                                        }).ToList();
+            ViewBag.drop2 = per;
+            //Musteriler
+            List<SelectListItem> must = (from x in db.tblmusteri.ToList()
+                                         select new SelectListItem
+                                         {
+                                             Text = x.ad + " " + x.soyad,
+                                             Value = x.id.ToString()
+                                         }).ToList();
+            ViewBag.drop3 = must;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniSatis(tblsatislar p)
+        {
+
+            var urun = db.tblurunler.Where(x => x.id == p.tblurunler.id).FirstOrDefault();
+            var musteri = db.tblmusteri.Where(x => x.id == p.tblmusteri.id).FirstOrDefault();
+            var personel = db.tblpersonel.Where(x => x.id == p.tblpersonel.id).FirstOrDefault();
+            p.tblurunler = urun;
+            p.tblmusteri = musteri;
+            p.tblpersonel = personel;
+            p.tarih =DateTime.Parse( DateTime.Now.ToShortDateString());
+            db.tblsatislar.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
